fa0e897e8450131062157931f11ae4f48c4caa24
prashantnitt07 <31304206+prashantnitt07@users.noreply.github.com>
2025-10-02 16:53:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,21 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.38"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.14"
+    }
+  }
+}
+
 provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+provider "kubectl" {
   config_path = "~/.kube/config"
 }
 
