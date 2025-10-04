96659c44a8cf528250bb4039b3612fcc2316bc31
prashantnitt07 <31304206+prashantnitt07@users.noreply.github.com>
2025-10-04 15:22:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,5 @@
 terraform {
   required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.38"
-    }
     kubectl = {
       source  = "gavinbunney/kubectl"
       version = "~> 1.14"
@@ -11,37 +7,34 @@ terraform {
   }
 }
 
-provider "kubernetes" {
-  config_path = "~/.kube/config"
-}
-
 provider "kubectl" {
   config_path = "~/.kube/config"
 }
 
 # Create monitoring namespace
-resource "kubernetes_namespace" "monitoring" {
-  metadata {
-    name = "monitoring"
-  }
+resource "kubectl_manifest" "monitoring_namespace" {
+  yaml_body = <<YAML
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: monitoring
+YAML
 }
 
 # Apply Prometheus ConfigMap YAML
 resource "kubectl_manifest" "prometheus_config" {
   yaml_body = file("${path.module}/manifests/prometheus-config.yaml")
-  depends_on = [kubernetes_namespace.monitoring]
+  depends_on = [kubectl_manifest.monitoring_namespace]
 }
 
 # Apply Prometheus Service YAML
 resource "kubectl_manifest" "prometheus_service" {
   yaml_body = file("${path.module}/manifests/prometheus-service.yaml")
-  depends_on = [
-    kubectl_manifest.prometheus_config
-  ]
+  depends_on = [kubectl_manifest.monitoring_namespace]
 }
 
 # Apply Prometheus Deployment YAML
-resource "kubectl_manifest" "prometheus" {
+resource "kubectl_manifest" "prometheus_deployment" {
   yaml_body = file("${path.module}/manifests/prometheus-deployment.yaml")
   depends_on = [
     kubectl_manifest.prometheus_config,
